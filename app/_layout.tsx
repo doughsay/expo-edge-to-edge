4ea222e9f31b49561635b49270fed2def8bd00b0
Chris Dosé <chris@xn--dos-dma.com>
2024-11-13 18:08:43 -0800

    minimal RNTP crash example

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,20 @@
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import "react-native-reanimated";
+import TrackPlayer from "react-native-track-player";
 
 export default function RootLayout() {
+  useEffect(() => {
+    TrackPlayer.setupPlayer()
+      .then(() => {
+        console.log("Player ready");
+      })
+      .catch((error) => {
+        console.log("Player setup error", error);
+      });
+  }, []);
+
   return (
     <ThemeProvider value={DarkTheme}>
       <Stack>
